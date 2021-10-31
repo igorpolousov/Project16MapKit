a7300bdd5c465c80cd13871374aae900bb2c55b5
Igor Polousov <igorpolousov@gmail.com>
2021-10-31 15:16:45 +0300

    Initial Commit

---
+++ Project16MapKit/Capital.swift
@@ -0,0 +1,12 @@
+//
+//  Capital.swift
+//  Project16MapKit
+//
+//  Created by Igor Polousov on 31.10.2021.
+//
+
+import UIKit
+
+class Capital: NSObject {
+
+}
---
+++ Project16MapKit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project16MapKit
+//
+//  Created by Igor Polousov on 31.10.2021.
+//
+import MapKit
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
